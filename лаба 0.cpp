5820c5cd114cd85947717f51f8509456ca7b8e89
Iaroslav2 <71449711+Iaroslav2@users.noreply.github.com>
2020-09-17 17:54:19 +0300

    Add files via upload

---
+++ лаба 0.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int
+main(void) {
+	int a, b, c, d = b + c;
+	printf("Input a");
+	scanf("%d,"&a);
+	printf("Input b");
+	scanf(%d,&b);
+	printf("Input c");
+	scanf(%d,&c);
+	printf(% d + % d = % d, b, c, d);
+		return 0
+}
+	
