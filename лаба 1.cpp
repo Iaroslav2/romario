28e78f89ff705410100929d1b72b85c3f29cf43c
Iaroslav2 <71449711+Iaroslav2@users.noreply.github.com>
2020-09-17 18:01:51 +0300

    Add files via upload

---
+++ лаба 1.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+int main() {
+	char k = 0;
+	char A[] = { 2,3,6,12,43,5,2,56,5,0};
+	for (char i = 0, i ,< 10 , (i++));
+	if (A[i] > k) {
+		k == A[i] };
+	printf(k);
+	return 0;
+}
